2f5ee2e50c67cffe57231b7bda5c2f32cd272b5c
Nithish Reddy <nithish.kottam@voltuswave.com>
2021-11-18 20:25:02 +0530

    my first chat appliction

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client';
+import {useState} from 'react';
+import Chat from './Chat';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const socket = io.connect("http://localhost:3001");
+
+function App() { 
+const [username, setUsername] = useState("");
+const [room, setRoom] = useState("");
+const [showChat, setshowChat] = useState(false);
+
+const joinRoom=()=>{
+   if(username !== "" && room !== "" ){
+      socket.emit("join_room",room)
+      setshowChat(true);
+   }
+}
+   return (
+        <div>
+           {
+              !showChat ? (
+
+            <div className="mfield">
+         <h1>Join A Chat</h1> 
+         <div>
+            <div className="field1">
+            <input type="text" className="infield1" value={username} onChange={(event)=>{setUsername(event.target.value)}} placeholder="jhon.."/>
+            </div>
+            <div className="field2">
+            <input type="text" className="infield2" value={room} onChange={(event)=>{setRoom(event.target.value)}} placeholder="RoomId.."/>
+            </div>
+            <div className="btn">
+            <button onClick={joinRoom} className="infield" >join a room</button>
+            </div>
+         </div>
+      </div>
+      ) :
+            (
+            <Chat socket={socket} username={username} room={room}/>
+             )}
+      </div>  
+   );
 }
 
-export default App;
+export default App
+
